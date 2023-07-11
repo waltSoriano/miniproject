4a9ffe5a2cacbc3f0b463f28e87871650c334d56
waltSoriano <135300851+waltSoriano@users.noreply.github.com>
2023-07-11 17:58:39 +0800

    navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-export default function App () {
-  return "Hi"
+import React from 'react';
+
+import NavigationBar from './components/navBar';
+
+
+const App =()=> {
   
+
+  return (
+    <>
+    <div><NavigationBar/></div>
+    </>
+   
+  );
 }
+
+export default App;
