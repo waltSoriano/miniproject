c32132f56a56ccc6e463754d54a7d621b3fd7e84
waltSoriano <135300851+waltSoriano@users.noreply.github.com>
2023-07-14 19:22:35 +0800

    changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='whole'>
+      <div className="background">
+        
+        </div>
+      </div>
+
+      
+    
+  )
+}
+
+export default App
